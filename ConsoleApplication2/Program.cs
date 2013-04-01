8f644f5a5fd309ad2fd996e53dc632ec2b5766b3
asniper <asniper@gmail.com>
2013-04-01 12:31:36 -0400

    Initial

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnection("http://localhost:53045/");
+
+            try
+            {
+                connection.StateChanged += connection_StateChanged;
+
+                var transport = new Microsoft.AspNet.SignalR.Client.Transports.LongPollingTransport();
+                connection.Start(transport).Wait();
+                Console.WriteLine("Connected");
+                Console.ReadLine();
+                //connection.Start().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("SignalRConnection:StartConnection ex: " + ex.Message);
+            }
+        }
+
+        static void connection_StateChanged(StateChange obj)
+        {
+            Console.WriteLine(DateTime.Now.TimeOfDay.ToString() + "state changed from " + obj.OldState.ToString() + " to " + obj.NewState.ToString());
+        }
+    }
+}
